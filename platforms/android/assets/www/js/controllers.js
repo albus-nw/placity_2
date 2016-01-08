cdf5d7dab738bba5e370eddda8cb2af53191973a
paul280 <paulkoch2805@web.de>
2016-01-08 03:10:35 +0100

    file plugin teilweise

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -85,6 +85,15 @@ playerName = null;
 controllers.controller("indexCtrl",['$scope', '$location', function ($scope, $location) {
     $scope.playerName = "";
     //hier: playerName aus datei lesen, wenn nicht vorhanden, auf login routen und datei schreiben dort
+    var data = readFromFile('profile.json');
+    var z = this;
+    console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<< adada   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><');
+    console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<< adada ' + data + '  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><');
+    if (data && data.playerName) {
+        console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<  '+data+'  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><');
+        $scope.playerName = data.playerName;
+        playerName = data.playerName;
+    }
     if (!playerName) {
         $location.path('/Login');
 
@@ -107,7 +116,8 @@ controllers.controller("indexCtrl",['$scope', '$location', function ($scope, $lo
 controllers.controller("loginCtrl", ['$scope', '$location', function ($scope, $location) {
     
     $scope.login = function () {
-        playerName = $scope.playerName + "";
+        //playerName = $scope.playerName + "";
+        writeToFile('profile.json', { playerName : 'dennis 23' });
         $location.path('/');
     };
     $scope.noLogin = function () {
@@ -231,3 +241,74 @@ controllers.controller('ScanCtrl', ['$rootScope', '$scope', '$location', functio
     console.log($scope.result + "wwwwwwwwwwwwww");
     
 }]);
+
+
+//in Dateisystem schreiben
+////https://www.neontribe.co.uk/cordova-file-plugin-examples/
+function writeToFile(fileName, data) {
+    data = JSON.stringify(data, null, '\t');
+    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+        directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+            fileEntry.createWriter(function (fileWriter) {
+                fileWriter.onwriteend = function (e) {
+                    // for real-world usage, you might consider passing a success callback
+                    console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>Write of file "' + fileName + '"" completed.');
+                };
+
+                fileWriter.onerror = function (e) {
+                    // you could hook this up with our global error handler, or pass in an error callback
+                    console.log('<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>Write failed: ' + e.toString());
+                };
+
+                var blob = new Blob([data], { type: 'text/plain' });
+                fileWriter.write(blob);
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }, errorHandler.bind(null, fileName));
+};
+
+//datei lesen
+function readFromFile(fileName) {
+    var pathToFile = cordova.file.dataDirectory + fileName;
+    window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+        fileEntry.file(function (file) {
+            var reader = new FileReader();
+
+            reader.onloadend = function (e) {
+                console.log('<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>vor dem retuern 256 >>>>>>>>>>>>>>akdgkg   ' + this.result);
+              playerName = this.result.playerName;
+                return JSON.parse(this.result);
+            };
+
+            reader.readAsText(file);
+        }, errorHandler.bind(null, fileName));
+    }, errorHandler.bind(null, fileName));
+};
+
+var errorHandler = function (fileName, e) {
+    var msg = '';
+
+    switch (e.code) {
+        case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'Storage quota exceeded';
+            break;
+        case FileError.NOT_FOUND_ERR:
+            msg = 'File not found';
+            break;
+        case FileError.SECURITY_ERR:
+            msg = 'Security error';
+            break;
+        case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'Invalid modification';
+            break;
+        case FileError.INVALID_STATE_ERR:
+            msg = 'Invalid state';
+            break;
+        default:
+            msg = 'Unknown error';
+            break;
+    };
+
+    console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>Error (' + fileName + '): ' + msg);
+};
+
