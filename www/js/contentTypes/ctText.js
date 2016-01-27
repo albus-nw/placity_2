c4b18de5f020d50442bcf07b272450319685c9d2
Phil <phwe@gmx.net>
2016-01-27 12:11:26 +0100

    ctText + scope: { } in ctImage

---
+++ www/js/contentTypes/ctText.js
@@ -0,0 +1,36 @@
+﻿/**
+ * ContentType Text, PW
+ */
+(function () {
+    'use strict';
+
+    angular
+      .module('contentTypes')
+      .directive('ctText', ctText);
+
+    ctText.$inject = [];
+    /* @ngInject */
+    function ctText() {
+        var directive = {
+            bindToController: true,
+            templateUrl: 'js/contentTypes/ctText.html',
+            controller: ctTextCtrl,
+            controllerAs: 'vm',
+            restrict: 'E',
+            scope: { }
+        };
+
+        return directive;
+    }
+
+
+    function ctTextCtrl() {
+        
+        var vm = this;
+        vm.data = {
+            text: '<p>Hier beindet sich html text</p> <p>der vom <h1>Nutzer</h1> als contenttype text angelegt wurde</p>'
+                    };
+
+    }
+})();
+
