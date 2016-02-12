61a3eb5ac8384917ca030471234f81fa172d4612
Phil <phwe@gmx.net>
2016-02-12 17:35:54 +0100

    buttons, in ct's auch in englisch

--- platforms/android/assets/www/js/contentTypes/ctFrageFreitext.js
+++ platforms/android/assets/www/js/contentTypes/ctFrageFreitext.js
@@ -30,28 +30,36 @@
     }
     ctFrageFreitextCtrl.$inject = ['$sce','$scope'];
     function ctFrageFreitextCtrl($sce,$scope) {
-        var lang_id;
+        
         var vm = this;
+        vm.lang_id;
         if ($scope.$parent.vm.lang_id) {
-            lang_id = $scope.$parent.vm.lang_id;
+            vm.lang_id = $scope.$parent.vm.lang_id;
 
         }
         var data_obj_parsed;
         data_obj_parsed = JSON.parse(vm.content.data_obj);
-        vm.ctFrageFreitextquestion = $sce.trustAsHtml(data_obj_parsed.languages[lang_id].fields[0].text);
-        vm.ctFrageFreitextanswer = data_obj_parsed.languages[lang_id].fields[0].answer;
+        vm.ctFrageFreitextquestion = $sce.trustAsHtml(data_obj_parsed.languages[vm.lang_id].fields[0].text);
+        vm.ctFrageFreitextanswer = data_obj_parsed.languages[vm.lang_id].fields[0].answer;
         
 
         vm.teste = function () {
             if (vm.ctFrageFreitextanswer == vm.answerGiven) {
-                //  console.log("Richtig!");
-                console.log(vm.answerGiven +"richtig");
-                vm.message = "Richtig";
+                            
+                if (vm.lang_id == '0') {
+                    vm.message = "Richtig";
+                }
+                else if (vm.lang_id == '1') {
+                    vm.message = "right";
+                }
             }
             else {
-               // console.log("Falsch!");
-                vm.message = "Falsch";
-                console.log(vm.answerGiven + "falsch");
+                if (vm.lang_id == '0') {
+                    vm.message = "Falsch";
+                }
+                else if (vm.lang_id == '1') {
+                    vm.message = "wrong";
+                }
             }
         }
     }
--- www/js/contentTypes/ctMultipleChoice.js
+++ www/js/contentTypes/ctMultipleChoice.js
@@ -29,30 +29,40 @@
 
     ctMultipleChoiceCtrl.$inject = ['$sce','$scope'];
     function ctMultipleChoiceCtrl($sce,$scope) {
-        var lang_id;
+        
         var vm = this;
+        vm.lang_id;
          if ($scope.$parent.vm.lang_id) {
-            lang_id = $scope.$parent.vm.lang_id;
+            vm.lang_id = $scope.$parent.vm.lang_id;
       
         }
         var data_obj_parsed;
         data_obj_parsed = JSON.parse(vm.content.data_obj);
-        vm.ctMultipleChoiceText = $sce.trustAsHtml(data_obj_parsed.languages[lang_id].fields[0].text);
+        vm.ctMultipleChoiceText = $sce.trustAsHtml(data_obj_parsed.languages[vm.lang_id].fields[0].text);
        //  vm.ctMultipleChoiceText = $sce.trustAsHtml(data_obj_parsed.languages[lang_id].fields[0].text);
-        vm.ctMultipleChoiceChoice = data_obj_parsed.languages[lang_id].fields[0].choice;
-        vm.ctMultipleChoiceChoice.answer = data_obj_parsed.languages[lang_id].fields[0].answer;
+        vm.ctMultipleChoiceChoice = data_obj_parsed.languages[vm.lang_id].fields[0].choice;
+        vm.ctMultipleChoiceChoice.answer = data_obj_parsed.languages[vm.lang_id].fields[0].answer;
       
         vm.answerGiven = "";
        
         vm.test = function () {
+            console.log(vm.lang_id);
             if (vm.answerGiven == vm.ctMultipleChoiceChoice.answer) {
-               // console.log("richtig");
-                vm.answerGiven = "Richtig!";
+                if (vm.lang_id == '0') {
+                    vm.answerGiven = "Richtig";
+                }
+                else if (vm.lang_id == '1') {
+                    vm.answerGiven = "right";
+                }
                 
                 }
             else {
-              //  console.log("falsch");
-                vm.answerGiven = "Falsch!";
+                if (vm.lang_id == '0') {
+                    vm.answerGiven = "Falsch";
+                }
+                else if (vm.lang_id == '1') {
+                    vm.answerGiven = "wrong";
+                }
             }
 
     }
